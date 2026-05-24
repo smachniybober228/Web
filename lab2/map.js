8f2d710c978bf679cf184f696ca27d2201a8a0ab
bobr <bobr@DESKTOP-K891U1S>
2026-05-24 19:20:01 +0700

    Now, when you click, the color of the button changes.

--- lab2/map.js
+++ lab2/map.js
@@ -1,3 +1,18 @@
+function changeActiveButton(newIndex) {
+    if (newIndex !== btnIndex) {
+        const oldBtn = document.querySelector(`#contacts .flex-row:last-child button:nth-of-type(${btnIndex})`);
+        const newBtn = document.querySelector(`#contacts .flex-row:last-child button:nth-of-type(${newIndex})`);
+        oldBtn.classList.remove('active-button');
+        newBtn.classList.add('active-button');
+        btnIndex = newIndex;
+    }
+}
+
+function btnFunc(newIndex, continentName){
+    changeActiveButton(newIndex);
+    fetchAndShow(continentName);
+}
+
 function showContinent(lat, lon, radius, color = "red") {
     if (currentCircle) {
         map.removeLayer(currentCircle);
@@ -6,7 +21,7 @@ function showContinent(lat, lon, radius, color = "red") {
     map.setView([lat, lon], 4); // приближаем к континенту
 }
 
-function fetchAndShow(continentName, radius = 1) {
+function fetchAndShow(continentName, radius = 1500000) {
     // 1. Формируем URL для запроса
     const url = `https://nominatim.openstreetmap.org/search?q=${continentName}&format=json`;
 
@@ -18,7 +33,7 @@ function fetchAndShow(continentName, radius = 1) {
             const lon = data[0].lon;            // долгота из ответа
         
             // 5. Рисуем окружность на карте
-            showContinent(lat, lon, 1500000);    
+            showContinent(lat, lon, radius);    
         })
         .catch(error => {                       // 6. Обрабатываем ошибки
             console.log('API недоступно, использую запасные координаты');
@@ -28,13 +43,16 @@ function fetchAndShow(continentName, radius = 1) {
 const map = L.map('map').setView([20, 0], 2);
 
 let currentCircle = null;
+let btnIndex = 1; // Индекс активной кнопки
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
 const buttons = document.querySelectorAll("#contacts .flex-row:last-child button")
-buttons[0].addEventListener("click", () => fetchAndShow("Europe"));
-buttons[1].addEventListener("click", () => fetchAndShow("Asia"));
-buttons[2].addEventListener("click", () => fetchAndShow("America"));
-buttons[3].addEventListener("click", () => fetchAndShow("Africa"));
+buttons[0].addEventListener("click", () => btnFunc(1, "Europe"));
+buttons[1].addEventListener("click", () => btnFunc(2, "Asia"));
+buttons[2].addEventListener("click", () => btnFunc(3, "America"));
+buttons[3].addEventListener("click", () => btnFunc(4, "Africa"));
+
+fetchAndShow("Europe");
