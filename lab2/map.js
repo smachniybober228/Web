41b12f00aad8b6c4bd642fef12426dbb3adfdd77
bobr <bobr@DESKTOP-K891U1S>
2026-05-24 18:24:28 +0700

    The map is now taken from the API instead of the SVG

---
+++ lab2/map.js
@@ -0,0 +1,5 @@
+const map = L.map('map').setView([20, 0], 2);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
