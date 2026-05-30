a5fce1e1fdacbd24885269a48144869d12949e7a
bobr <bobr@DESKTOP-K891U1S>
2026-05-30 20:47:08 +0700

    Added reverse geocoding

--- lab10/geo.js
+++ lab10/geo.js
@@ -1,3 +1,4 @@
+// Moscow coords
 const DefaultLat = 55.7522;
 const DefaultLon = 37.6156;
 
@@ -13,26 +14,49 @@ function getCurrentGeolocation()
 				const lat = position.coords.latitude;
 				const lon = position.coords.longitude;
 				resolve({lat, lon});
-			}, (error) =>
+			}, (error) => {
+                // test
+                // const lat = 55.01
+				// const lon = 82.55
+                // resolve({lat, lon})
                 reject(error)
+            }
             );
 		} else
             reject('Геолокация не поддерживается этим браузером.');
 	});
 }
 
-function changeCity(coords){
+function changeCity(text){
     const citySpan = document.getElementById("city-name");
-    if (!citySpan) return;
-
-    citySpan.textContent = `lat: ${coords.lat}; lon: ${coords.lon}`;
+    if (citySpan) citySpan.textContent = text;
 }
 
+async function getCityByCoords(lat, lon) {
+    const url = `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json&accept-language=ru`;
+    const response = await fetch(url);
+    const data = await response.json();
+    const addr = data.address;
+
+    let cityName = addr.city || addr.town || addr.village || addr.state || 'Неизвестное место';
+    if ((addr.village || addr.hamlet) && !addr.city && !addr.town) {
+        cityName = `${addr.village || addr.hamlet} (${addr.state || addr.region || addr.country})`;
+    }
+    return cityName;
+}
+
+
 getCurrentGeolocation()
-    .then(coords => changeCity(coords))
-    .catch(err => 
+    .then(async coords => {
+        const city = await getCityByCoords(coords.lat, coords.lon);
+        changeCity(city);
+    })
+    .catch(async err => 
     {
-        if (err.code === 1){
+        if (err === "Геолокация не поддерживается этим браузером."){
+            console.log(err);
+        }
+        else if (err.code === 1){
             console.log("Доступ к геолокации запрещён.");
         }
         else if (err.code === 2){
@@ -43,8 +67,7 @@ getCurrentGeolocation()
         }
         console.log("Использован город по умолчанию.")
 
-        const lat = DefaultLat;
-        const lon = DefaultLon;
-        changeCity({lat, lon});
+        const city = await getCityByCoords(DefaultLat, DefaultLon);
+        changeCity(city);
     }
 )
