b20abf3db4895d060f30ba8f48600874e3aebec9
bobr <bobr@DESKTOP-K891U1S>
2026-06-07 20:14:14 +0700

    CreateButton is working

--- lab11/lab11.js
+++ lab11/lab11.js
@@ -1,5 +1,35 @@
+const textField = document.getElementById("NewElementName");
+
+const clearTextField = () => textField.value = "";
+
 const clearBtn = document.getElementById("clearButton");
-clearBtn.addEventListener("click", function() {
-    const textField = document.getElementById("NewElementName");
-    textField.value = "";
+clearBtn.addEventListener("click", clearTextField);
+
+const createBtn = document.getElementById("createButton");
+createBtn.addEventListener("click", function() {
+    if (textField.value === "") return;
+
+    const today = new Date();
+    const day = String(today.getDate()).padStart(2, '0');
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const year = today.getFullYear();
+    const todayFrmt = `от ${day}.${month}.${year}`;
+    console.log(todayFrmt);
+
+    todoList = document.getElementById("TodoList");
+    todoList.innerHTML += `
+        <li>
+            <label class="MyLabel">
+                <input type="checkbox" class="CustomCheckbox">
+                <span class="Checkmark"></span>
+                <span class="AllText">
+                <span class="TaskText">${textField.value}</span>
+                <sub class="SubTaskText">${todayFrmt}</sub>
+                </span>
+            </label>
+            <button class="MyButton">✖</button>
+        </li>
+    `;
+
+    clearTextField();
 });
